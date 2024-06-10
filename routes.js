ca3ccd2e7b0557ae2c0af017191a50a317c91504
Brenda Armstrong <137068502+brendaleearmstrong@users.noreply.github.com>
2024-06-10 11:36:06 -0230

    Ensure routes serve HTML files from views folder

--- routes.js
+++ routes.js
@@ -8,13 +8,13 @@ const handleRequest = (req, res) => {
 
     fs.readFile(filePath, (err, data) => {
         if (err) {
-            logEvent('404 Not Found', req.url);
+            logEvent('404 Not Found', req.url, 'File not found');
             res.writeHead(404, { 'Content-Type': 'text/html' });
             fs.readFile(path.join(__dirname, 'views', '404.html'), (error, notFoundData) => {
                 res.end(notFoundData);
             });
         } else {
-            logEvent('200 OK', req.url);
+            logEvent('200 OK', req.url, 'File served successfully');
             res.writeHead(200, { 'Content-Type': 'text/html' });
             res.end(data);
         }
@@ -22,3 +22,4 @@ const handleRequest = (req, res) => {
 };
 
 module.exports = { handleRequest };
+
